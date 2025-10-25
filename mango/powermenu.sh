860f0f95c78aa7579c839f85d5cd79acf51e51db
gabriel <49964470+369gabriel@users.noreply.github.com>
2025-10-25 19:00:45 -0300

    Add files via upload

---
+++ mango/powermenu.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+opcoes="󰐥  Shutdown\n  Reboot\n  Lock\n  Exit\n  Exit"
+
+escolha=$(echo -e "$opcoes" | wofi -s /home/gabriel/.config/wofi/style.css --show dmenu --prompt "Power Menu" --width 300 --height 230)
+
+case "$escolha" in
+    "󰐥  Shutdown")
+        poweroff
+        ;;
+    "  Reboot")
+        reboot
+        ;;
+    "  Lock")
+        swaylock
+        ;;
+    "  Exit")
+        hyprctl dispatch exit
+        ;;
+    "  Exit")
+        loginctl suspend
+        ;;
+    *)
+        ;;
+esac
